e5523b715259440a9deacfc44ec8ddfafdd13edd
Random <rc786986@gmail.com>
2023-11-29 15:40:49 +0530

    finished 6 chapter of CHAI AUR JS

---
+++ conversionOperations.js
@@ -0,0 +1,56 @@
+// let score = "33"
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = "33abc"
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = null
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = undefined
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = true
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = "RAHUL"
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+
+// NaN =>   Not an NUmber
