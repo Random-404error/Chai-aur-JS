f8e4a278ebaafb8c870147ef11db368633878aa5
Random <rc786986@gmail.com>
2024-01-07 13:40:48 +0530

    adding

---
+++ nums_math.js
@@ -0,0 +1,5 @@
+const score = 100
+// console.log(score)
+
+const balance = new Number(150)
+console.log(balance)
---
+++ string.js
@@ -0,0 +1,37 @@
+const name = "Rahul"
+const repoCount = 10
+
+// console.log(name + " " + repoCount + " Value")
+
+// console.log(`Hello my name is ${name}  and my repo count is ${repoCount}`)
+
+const gameName = new String('Rahul_Choudhary')
+
+// console.log(gameName)
+
+// console.log(gameName[2])
+
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(6))
+
+// console.log(gameName.indexOf('o'))
+
+const newName = gameName.substring(0, 4)
+// console.log(newName)
+
+const newString = gameName.slice(-15, 15)
+// console.log(newString)
+
+const newGame = "   hi      "
+// console.log(newGame)
+// console.log(newGame.trim())
+
+const url = "https://Rahul.com/RK%20Choudhary"
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('Rahul'))
+
+// console.log(gameName.split('_'))
